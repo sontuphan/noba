ce21e803f2842142bf4409f6b97865114fa8e7f6
Tu Phan <tu.phan@tether.io>
2025-09-21 15:26:02 +0700

    chore: add comments

--- src/index.ts
+++ src/index.ts
@@ -5,26 +5,35 @@ import { uuid } from './utils'
 
 if (!process.env.NOBA_MAIN_ID) process.exit(1)
 
+/**
+ * Coverage in Node
+ */
+if (!!process.env.NODE_V8_COVERAGE) {
+  // Ignored. V8 Engine auto detect this flag.
+}
+
+/**
+ * Coverage in Bare
+ */
 if (!!process.env.NOBA_BARE_COVERAGE) {
   const dir = process.env.NOBA_BARE_COVERAGE
-
   require('bare-cov')({ dir })
 
   process.once('exit', () => {
     const v8Json = dir + '/coverage-final.json'
-
     if (existsSync(v8Json)) rmSync(v8Json)
 
     const oldDump = `${dir}/v8-coverage.json`
     const newDump = `${dir}/coverage-${uuid(5)}-${Date.now()}.json`
-
     if (existsSync(oldDump)) renameSync(oldDump, newDump)
   })
 }
 
+/**
+ * Noba modules
+ */
+export * from './utils'
 export const { describe, test, it } = new Runner(
   process.env.NOBA_MAIN_ID,
   Number(process.env.NOBA_TIMEOUT) || undefined,
 )
-
-export * from './utils'
